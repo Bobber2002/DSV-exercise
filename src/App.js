022638dc6e6109ebc2302260101b8146f4d3f5a3
ChristofferRTS <4906240@RTS-365.dk>
2022-02-15 17:12:35 +0100

    import users and useEffect

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
 import "./styles.css";
-import { useReducer, useRef, useState } from "react";
-import users from "./data";
+import { useReducer, useRef, useState, useEffect } from "react";
 
 /** Instructions
-   0. Fork this codesandbox and sync it with your github 
-   1. import users data from data.ts
-   2. Using the useEffect hook:
-   2.1. Filter the users data array to only include users where age >= 18
-   2.2. Map the users data array to only include username, address, age and companyName
-   2.3. Dispatch the data to the local users state
-   3. Display the users' properties using a loop in the JSX
-   4. Extend the reducer:
-   4.1. Count must always be >= 0, in all cases
-   4.2. Add a case to increment count with 2
-   4.3. Add a case to increment to the nearest odd number
-   4.4. Add a case to reset the count
-   4.5. Add buttons to said cases
-   5. Add a new local state instance called userCoordinates, with an initialState of 0
-   5.1. Set up a new useEffect hook with a dependency to users
-   5.2. Map the users data array to only include address.geo.lng, as a number
-   5.3. Using the .reduce method, sum the array and dispatch the outcome to the local state
-   5.4. Print userCoordinates to the console
-   6. Using the useRef hook and useEffect hook, auto-focus the input field on load
-   7. Add an onChange event to the input, which updates the local state of text
-   8. Provide the link to your forked repo with your answers
-   */
+ 0. Fork this codesandbox and sync it with your github 
+ 1. import users data from data.ts
+ 2. Using the useEffect hook:
+ 2.1. Filter the users data array to only include users where age >= 18
+ 2.2. Map the users data array to only include username, address, age and companyName
+ 2.3. Dispatch the data to the local users state
+ 3. Display the users' properties using a loop in the JSX
+ 4. Extend the reducer:
+ 4.1. Count must always be >= 0, in all cases
+ 4.2. Add a case to increment count with 2
+ 4.3. Add a case to increment to the nearest odd number
+ 4.4. Add a case to reset the count
+ 4.5. Add buttons to said cases
+ 5. Add a new local state instance called userCoordinates, with an initialState of 0
+ 5.1. Set up a new useEffect hook with a dependency to users
+ 5.2. Map the users data array to only include address.geo.lng, as a number
+ 5.3. Using the .reduce method, sum the array and dispatch the outcome to the local state
+ 5.4. Print userCoordinates to the console
+ 6. Using the useRef hook and useEffect hook, auto-focus the input field on load
+ 7. Add an onChange event to the input, which updates the local state of text
+ 8. Provide the link to your forked repo with your answers
+ */
+
+import users from "./data";
+
+const data = users;
 
 function reducer(state, action) {
   switch (action.type) {
@@ -37,11 +40,16 @@ function reducer(state, action) {
   }
 }
 
+
 export default function App() {
   const [users, setUsers] = useState([]);
   const [text, setText] = useState("");
   const [countState, dispatch] = useReducer(reducer, { count: 0 });
   const ref = useRef(null);
+  
+  useEffect(() => {
+      console.log(data);
+  }, []);
 
   return (
     <div className="App">
